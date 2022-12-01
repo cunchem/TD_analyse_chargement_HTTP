76615c2495dfab3031aa8b08f44c9ea948c6833f
cunche <mathieu.cunche@inria.fr>
2022-12-01 17:14:00 +0100

    Added the files

---
+++ analyse_fichier_HAR.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec  1 10:48:05 2022
+
+@author: cunche
+"""
+
+import IP2Location # pour géolocaliser un serveur à partir d'une adresse IP
+import json # necessaire pour HarPage
+from haralyzer import HarPage # pour lire le fichier .har
+import numpy as np #  pour l'analyse de données
+import matplotlib.pyplot as plt #  pour créer des graphs
+import pandas as pd # pour l'analyse de données
+
+
+
+# Initialisation des bases IP2Location
+baseIPV4 = IP2Location.IP2Location("IP2LOCATION-LITE-DB5.BIN")
+baseIPV6 = IP2Location.IP2Location("IP2LOCATION-LITE-DB5.IPV6.BIN")
+ipTools = IP2Location.IP2LocationIPTools()
+
+
+# vvvv Ne pas modifier vvvv:
+def plot_data(my_data) :
+    my_array = np.array(my_data)  
+    fig, axes = plt.subplots(nrows=3, ncols=2, constrained_layout = True)
+    
+    df = pd.DataFrame(my_array, columns = ['hostname','tld','domain','requestSize', 'responseSize', 'country'])
+    df=df.astype({'requestSize' : 'int64', 'responseSize' : 'int64'})
+    print("="*20)
+    # nb de domaines de second niveau contactés
+    nb_domain = len(pd.unique(df["domain"]))
+    print(f"Nombre de domaine de second niveau : {nb_domain}")    
+    
+    # Volume de données envoyé par pays
+    df.groupby("country")["requestSize"].sum().sort_values(ascending=True).plot(kind='barh',ax=axes[0,0],title = "Volume envoyé par pays")
+    # Volume de données recues par pays
+    df.groupby("country")["responseSize"].sum().sort_values(ascending=True).plot(kind='barh',ax=axes[0,1],title = "Volume reçu par pays")
+    # Volume de données envoyé par domaine de 2nd niveau
+    df.groupby("domain")["requestSize"].sum().sort_values(ascending=True).tail(15).plot(kind='barh',ax=axes[1,0],title = "Volume envoyé par domaine 2nd niv")
+    # Volume de données recues par domaine de 2nd niveau
+    df.groupby("domain")["responseSize"].sum().sort_values(ascending=True).tail(15).plot(kind='barh',ax=axes[1,1],title = "Volume reçu par domaine 2nd niv")
+    # Nombre d'échanges par pays
+    df.groupby("country")["country"].count().sort_values(ascending=True).plot(kind='barh',ax=axes[2,0],title = "Nb. échanges par pays")
+    # Nombre d'échanges par domaine de 2nd niveau
+    df.groupby("domain")["domain"].count().sort_values(ascending=True).tail(15).plot(kind='barh',ax=axes[2,1],title = "Nb. échanges par domaine 2nd niv")
+    
+
+
+        
+def analyse_entry(entry):
+    print("=============================")
+
+    # Liste des variables à renseigner
+    hostname = ""
+    tld = "" # Top Level Domain
+    domain_2 = "" # Domaine de second niveau
+    requestSize = 0 # Taille de la requete
+    responseSize = 0 # Taille de la réponse
+    country = "" # code du pays ou se trouve le serveur
+    
+    # Récupération de l'IP du serveur :
+    try :
+        ipServeur = entry.serverAddress
+    except KeyError :
+        ipServeur = "0.0.0.0" # mettre à une valeur par défaut si l'attribut est absent
+        
+    # TODO : récupérer / calculer la valeurs des variables précédentes
+    
+    # Hostname
+    hostname = entry.request.host
+    print(f"hostname = {hostname}")
+    # TLD et domain_2
+    tld = hostname.split('.')[-1]
+    print(f"TLD = {tld}")
+    domain_2 = hostname.split('.')[-2] + "." + tld
+    print(f"domain de second niveau = {domain_2}")
+    # requestSize et responseSize
+    requestSize = entry.request.bodySize
+    responseSize = entry.response.bodySize
+    # country code
+    
+    if ipTools.is_ipv4(ipServeur) :
+        rec = baseIPV4.get_all(ipServeur) #https://www.ip2location.com/development-libraries/ip2location/python
+    else :
+        rec = baseIPV6.get_all(ipServeur)
+    country = rec.country_short 
+    
+    # Les résultats sont rangés dans une liste avant d'être retournés
+    res = [hostname, tld, domain_2, requestSize, responseSize, country]
+    return res    
+
+
+# TODO : renseigner le nom du fichier du journal HAR a ouvrir
+with open("har_data.har", 'r') as f:
+    har_page = HarPage('page_3', har_data=json.loads(f.read())) # en cas d'erreur avec le page_id, modifier en "page_1"
+
+
+my_data = []
+for e in har_page.entries:
+   a = analyse_entry(e)
+   print(a)
+   my_data += [a]
+plot_data(my_data)
+
---
+++ etapes_preliminaires.py
@@ -0,0 +1,70 @@
+import IP2Location # pour obtenir la geoloc à partir d'une adresse IP
+import json # necessaire pour HarPage
+from haralyzer import HarPage # pour l'analyse de fichiers HAR
+
+
+hostname = "www.sydney.edu.au"
+ipv6_server = "2606:4700::6812:1ef8" # ici en IPV6
+
+
+#  Récupération du TLD (Top Level Domain) et du domaine à partir d'un nom
+# Utiliser le découpage d'une chaine à l'aide de la fonction split
+# Dans www.elysee.fr le tld est fr et le domaine est elysee.fr
+tld = hostname.split('.')[-1]
+print(f"TLD = {tld}")
+domain_2 = hostname.split('.')[-2] + "." + tld
+print(f"domaine de second niveau = {domain_2}")
+
+# Recupération des coordonnées géographiques d'un serveur
+# On va utiliser IP2Location 
+# Dans un premier temps on doit initialiser les bases de geolocalisation (IPV4 et IPV6)
+# On doit préciser les fichiers contenant les données de geoloc de ces bases (ils doivent etre dans le le meme repertoire que le programme)
+baseIPV4 = IP2Location.IP2Location("IP2LOCATION-LITE-DB5.BIN")
+baseIPV6 = IP2Location.IP2Location("IP2LOCATION-LITE-DB5.IPV6.BIN")
+# On va également avoir besoin d'utiliser l'utilitaire ipTools
+ipTools = IP2Location.IP2LocationIPTools()
+ 
+# IP2Location stocke les information de geoloc sous forme d'enregistrementn : une structure contenant plusieurs champs : lat, lng, country ..
+# La doc https://www.ip2location.com/development-libraries/ip2location/python
+
+# Il faut distinguer les cas en fonction du type d'adresse IP (v4 ou v6) (en utilisant ipTools)
+if ipTools.is_ipv4(ip_serveur) : 
+    rec = baseIPV4.get_all(ip_server) # Si l'adresse est en IPV4 on récupère l'enregistrement via la baseIPV4 en utilisant la fonction getall() 
+else :
+    rec = baseIPV6.get_all(ip_server) # Sinon (l'adresse est en IPV6) on récupère l'enregistrement sur la baseIPV6
+    
+# On dispose maintenant de l'enregistrement "rec" correspondant à l'ip
+# On peut maintenant accéder aux différents champs de l'enregistrement
+lat = rec.latitude # latitude
+lng = rec.longitude # longitude
+print(f"lat,lng : {lat},{lng}")
+# il existe un enregistrement "country_short" qui correspond au code court du pays
+country = rec.country_short
+print(f"code pays : {country}")
+
+
+# Lecture du fichier HAR
+# Un fichier HAR est structuré comme une liste d'entrée (record). Chaque entrée correspond à un échange réseau, et donc à une ligne dans la console du navigateur
+
+# On ouvre un fichier HAR en utilisant la fonction open  
+with open("example.har", 'r') as f:
+    # On charge ensuite la structure dans la variable har_page à l'aide de la fonction HarPage  
+    har_page = HarPage('page_1', har_data=json.loads(f.read())) # Attention le parametre page_1 peut avoir une autre valeur (généralement page_3)
+
+# On parcours ensuite la structure entrée par entrée
+for e in har_page.entries:
+    print("== entrée HAR : ==")
+    # chaque entrée e correspond à record représentant une interaction réseau
+    # une entrée est composée de différents attributs représentant les caractéristiques de l'échange
+    # par exemple l'attribut  e.url contient l'URL demandée
+    url = e.url
+    print(f"URL : {url}")
+    # Il existe d'autres attributs https://haralyzer.readthedocs.io/en/latest/basic/harentry.html
+    # Affichez les attributs suivants : addresse IP du serveur, port, nom de l'hote (serveur) 
+    print(e.serverAddress)
+    print(e.port)
+    print(e.request.host) # Attention l'attribut e.hostname correspond au hostname de la page demandé
+    
+
+
+    
